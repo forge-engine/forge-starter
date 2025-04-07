05763bd6be374fa9b215a837077ba6b78d101200
Jeremias Nunez <jeremias2@gmail.com>
2025-04-07 10:50:57 -0400

    updated starter

--- forge.php
+++ forge.php
@@ -1,14 +1,17 @@
 #!/usr/bin/env php
 <?php
+
 declare(strict_types=1);
 
 define("BASE_PATH", __DIR__);
+require_once BASE_PATH . "/engine/Core/Support/helpers.php";
 
 use Forge\Core\Bootstrap\Bootstrap;
 use Forge\Core\DI\Container;
 use Forge\CLI\Application;
 use Forge\Core\Autoloader;
 use Forge\Core\Config\EnvParser;
+use Forge\Core\Debug\Metrics;
 
 require BASE_PATH . "/engine/Core/Autoloader.php";
 require BASE_PATH . "/engine/Core/Config/EnvParser.php";
@@ -17,8 +20,9 @@ Autoloader::register();
 EnvParser::load(BASE_PATH . "/.env");
 
 $container = Container::getInstance();
+Metrics::start('cli_resolution');
 $container = Bootstrap::initCliContainer();
 
 $app = $container->get(Application::class);
-
+Metrics::stop('cli_resolution');
 exit($app->run($argv));
